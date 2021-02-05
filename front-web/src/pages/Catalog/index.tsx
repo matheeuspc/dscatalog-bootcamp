a95e113c7c85f2b46c072eb401fca9c0d6287c14
Matheus Cardoso <matheussp.cardoso@gmail.com>
2021-02-05 19:01:13 -0300

    Finish product listing layout

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,8 +1,22 @@
+import ProductCard from './components/ProductCard';
 import './styles.scss';
 
 const Catalog = () => (
     <div className="catalog-container">
         <h1 className="catalog-title">Catálogo de produtos</h1>
+        <div className="catalog-products">
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+        </div>
     </div>
 );
 
