82a1d9bbb46a782ee549661f4ccbe8d210e4d5d5
Matheus Cardoso <matheussp.cardoso@gmail.com>
2021-02-07 18:09:22 -0300

    Add base admin form structure

---
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -0,0 +1,19 @@
+import BaseForm from '../../BaseForm';
+import './styles.scss';
+
+const Form = () => {
+    return (
+        <BaseForm title="cadastrar um produto">
+            <div className="row">
+                <div className="col-6">
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col-6">
+
+                </div>
+            </div>
+        </BaseForm>
+    );
+}
+
+export default Form;
