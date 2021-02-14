57f7d8c0e574da165fc06363965f95f8f547d38d
Matheus Cardoso <matheussp.cardoso@gmail.com>
2021-02-13 21:21:11 -0300

    basic bean validation user and product

--- backend/src/main/java/com/mcardoso/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/mcardoso/dscatalog/dto/UserDTO.java
@@ -3,6 +3,9 @@ package com.mcardoso.dscatalog.dto;
 import com.mcardoso.dscatalog.entities.Role;
 import com.mcardoso.dscatalog.entities.User;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
@@ -11,8 +14,12 @@ public class UserDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
+
+    @NotBlank(message = "Campo obrigatório")
     private String firstName;
     private String lastName;
+
+    @Email(message = "Favor colocar um email válido")
     private String email;
 
     Set<RoleDTO> roles = new HashSet<>();
