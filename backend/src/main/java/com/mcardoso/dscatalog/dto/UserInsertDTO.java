5309bc2c746d4144b0dcf4e7d1778a6d2b2a25c2
Matheus Cardoso <matheussp.cardoso@gmail.com>
2021-02-13 22:09:18 -0300

    Custom constraintValidator

--- backend/src/main/java/com/mcardoso/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/mcardoso/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.mcardoso.dscatalog.dto;
 
+import com.mcardoso.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
     private static final long serialVersionUID = 1L;
 
---
+++ backend/src/main/java/com/mcardoso/dscatalog/services/validation/UserInsertValidator.java
@@ -0,0 +1,44 @@
+package com.mcardoso.dscatalog.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.mcardoso.dscatalog.dto.UserInsertDTO;
+import com.mcardoso.dscatalog.entities.User;
+import com.mcardoso.dscatalog.repositories.UserRepository;
+import com.mcardoso.dscatalog.resources.exceptions.FieldMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void initialize(UserInsertValid ann) {
+    }
+
+    @Override
+    public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        // Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à lista
+
+        User user = userRepository.findByEmail(dto.getEmail());
+
+        if (user != null) {
+            list.add(new FieldMessage("email", "Email já existe"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
