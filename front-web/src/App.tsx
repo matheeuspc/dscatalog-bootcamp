d1da468cf17619a6e0802addb61cd93d8aea9613
Matheus Cardoso <matheussp.cardoso@gmail.com>
2021-03-18 13:43:39 -0300

    Add toast when save product

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,17 @@
 import './core/assets/styles/custom.scss';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './app.scss';
 import Routes from './Routes';
 
 const App = () => {
 
   return (
-    <Routes />
+    <>
+      <Routes />
+      <ToastContainer />
+    </>
   );
-  }
+}
 
 export default App;
--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,6 +1,8 @@
 
 import { makePrivateRequest } from 'core/utils/request';
 import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router';
+import { toast } from 'react-toastify';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
 
@@ -13,9 +15,17 @@ type FormState = {
 
 const Form = () => {
     const { register, handleSubmit, errors } = useForm<FormState>();
+    const history = useHistory();
 
     const onSubmit = (data: FormState) => {
-        makePrivateRequest({ url: '/products', method: 'POST', data });
+        makePrivateRequest({ url: '/products', method: 'POST', data })
+            .then(() => {
+                toast.info('Produto salvo com sucesso!');
+                history.push('/admin/products');
+            })
+            .catch(() => {
+                toast.error('Erro ao salvar produto!');
+            })
     }
 
     return (
