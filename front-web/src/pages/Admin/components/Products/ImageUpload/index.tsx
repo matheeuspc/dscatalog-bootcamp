efd281a49ad62affb29135ddae38a9dc670b5d80
Matheus Cardoso <matheussp.cardoso@gmail.com>
2021-03-22 09:14:53 -0300

    upload image to s3

--- front-web/src/pages/Admin/components/Products/ImageUpload/index.tsx
+++ front-web/src/pages/Admin/components/Products/ImageUpload/index.tsx
@@ -1,8 +1,34 @@
 
 import './styles.scss';
 import { ReactComponent as UploadPlaceholder} from 'core/assets/images/upload-placeholder.svg';
+import { makePrivateRequest } from 'core/utils/request';
 
 const ImageUpload = () => {
+    const uploadImage = (selectedImage: File) => {
+        const payload = new FormData();
+        payload.append('file', selectedImage)
+
+        makePrivateRequest({ 
+            url: '/products/image', 
+            method: 'POST',
+            data: payload 
+        })
+        .then(() => {
+            console.log('arquivo enviado com sucesso');
+        })
+        .catch(() => {
+            console.log('erro ao enviar arquivo');
+        })
+    }
+
+    const handleChange = ( event: React.ChangeEvent<HTMLInputElement> ) => {
+        const selectedImage = event.target.files?.[0];
+
+        if (selectedImage) {
+            uploadImage(selectedImage);
+        }
+    }
+
     return (
         <div className="row">
             <div className="col-6">
@@ -10,6 +36,8 @@ const ImageUpload = () => {
                     <input 
                         type="file"
                         id="upload"
+                        accept="image/png, image/jpg"
+                        onChange={handleChange}
                         hidden
                     ></input>
                     <label htmlFor="upload">ADICIONAR IMAGEM</label>
