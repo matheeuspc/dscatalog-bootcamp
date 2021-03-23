0f21986ebd3ad74950fcb9320e5af857fb3cfdbf
Matheus Cardoso <matheussp.cardoso@gmail.com>
2021-03-23 00:04:02 -0300

    filter by name implementation

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,36 +1,42 @@
 import ProductCard from './components/ProductCard';
 import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './styles.scss';
 import { makeRequest } from 'core/utils/request';
 import { ProductsResponse } from 'core/types/Product';
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import Pagination from 'core/components/Pagination';
-import ProductFilters from 'core/components/ProductFilters';
+import ProductFilters, { FilterForm } from 'core/components/ProductFilters';
 
 const Catalog = () => {
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
     const [isLoading, setIsLoading] = useState(false);
     const [activePage, setActivePage] = useState(0);
 
-    useEffect(() => {
+    const getProducts = useCallback((filter?: FilterForm) => {
         const params = {
             page: activePage,
-            linesPerPage: 12
+            linesPerPage: 12,
+            name: filter?.name,
+            categoryId: filter?.categoryId
         }
         setIsLoading(true);
         makeRequest({ url: '/products', params })
             .then(response => setProductsResponse(response.data))
             .finally(() => {
                 setIsLoading(false);
-            });
+            })
     }, [activePage]);
+
+    useEffect(() => {
+        getProducts();
+    }, [getProducts]);
 
     return (
         <div className="catalog-container">
             <div className="d-flex justify-content-between">
                 <h1 className="catalog-title">Catálogo de produtos</h1>
-                <ProductFilters />
+                <ProductFilters onSearch={filter => getProducts(filter)} />
             </div>
             <div className="catalog-products">
                 {isLoading ? <ProductCardLoader /> : (
@@ -42,8 +48,8 @@ const Catalog = () => {
                 )}
             </div>
             {productsResponse && (
-                <Pagination 
-                    totalPages={productsResponse?.totalPages} 
+                <Pagination
+                    totalPages={productsResponse?.totalPages}
                     activePage={activePage}
                     onChange={page => setActivePage(page)}
                 />
