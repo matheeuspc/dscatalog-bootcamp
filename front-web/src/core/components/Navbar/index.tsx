773909a75ff1a2165de33884af25a093ec54a504
Matheus Cardoso <matheussp.cardoso@gmail.com>
2021-03-24 14:03:32 -0300

    add menu icon to responsive mobile

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -2,8 +2,11 @@ import { Link, NavLink, useLocation } from 'react-router-dom';
 import { getAccessTokenDecoded, logout } from 'core/utils/auth';
 import './styles.scss';
 import { useEffect, useState } from 'react';
+import menu from 'core/assets/images/menu.svg';
+
 
 const Navbar = () => {
+    const [drawerActive, setDrawerActive] = useState(false);
     const [currentUser, setCurrentUser] = useState('');
     const location = useLocation();
 
@@ -19,40 +22,72 @@ const Navbar = () => {
 
     return (
         <nav className="bg-primary main-nav">
-                <Link to="/" className="nav-logo-text">
-                    <h4>DS Catalog</h4>
-                </Link>
-            <div className="menu-container">
+            <Link to="/" className="nav-logo-text">
+                <h4>DS Catalog</h4>
+            </Link>
+            <button className="menu-mobile-btn" type="button" onClick={() => setDrawerActive(!drawerActive)}>
+                <img src={menu} alt="Mobile menu" />
+            </button>
+            <div className={drawerActive ? "menu-mobile-container" : "menu-container"}>
                 <ul className="main-menu">
                     <li>
-                        <NavLink to="/" exact className="nav-link">HOME</NavLink>
+                        <NavLink to="/" exact className="nav-link" onClick={() => setDrawerActive(false)}>HOME</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/products" className="nav-link">CATÁLOGO</NavLink>
+                        <NavLink to="/products" className="nav-link" onClick={() => setDrawerActive(false)}>CATÁLOGO</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/admin" className="nav-link">ADMIN</NavLink>
+                        <NavLink to="/admin" className="nav-link" onClick={() => setDrawerActive(false)}>ADMIN</NavLink>
                     </li>
+                    {drawerActive && (
+                        <li>
+                            {
+                                currentUser && (
+                                    <a href="#logout" className="nav-link-active d-inline" onClick={() => setDrawerActive(false)}>
+                                        {`LOGOUT - ${currentUser}`}
+                                    </a>
+                                )
+                            }
+                        </li>
+                    )}
+                    {
+                        drawerActive && (
+                            <>
+                                {
+                                    !currentUser && (
+                                        <li>
+                                            <Link to="/auth/login" className="nav-link active" onClick={() => setDrawerActive(false)}>
+                                                LOGIN
+                                            </Link>
+                                        </li>
+                                    )
+                                }
+                            </>
+                        )
+                    }
                 </ul>
             </div>
-            <div className="text-right">
-                { currentUser && (
-                        <>
-                            {currentUser}
-                            <a 
-                                href="#logout" 
-                                className="nav-link active d-inline"
-                                onClick={handleLogout}
-                            >
-                                LOGOUT
+            <div className="user-info-dnone text-right">
+                {currentUser && (
+                    <>
+                        {currentUser}
+                        <a
+                            href="#logout"
+                            className="nav-link active d-inline"
+                            onClick={(event) => {
+                                setDrawerActive(false);
+                                handleLogout(event);
+                            }}
+                        >
+                            LOGOUT
                             </a>
-                        </>
-                    )}
-                { !currentUser && (
-                        <Link to="/auth/login" className="nav-link active">
-                            LOGIN
-                        </Link>
-                    )}
+                    </>
+                )}
+                {!currentUser && (
+                    <Link to="/auth/login" className="nav-link active" onClick={() => setDrawerActive(false)}>
+                        LOGIN
+                    </Link>
+                )}
             </div>
         </nav>
     )
--- front-web/src/pages/Admin/components/Products/Card/index.tsx
+++ front-web/src/pages/Admin/components/Products/Card/index.tsx
@@ -11,8 +11,7 @@ type Props = {
 const Card = ({ product, onRemove } : Props) => {
     return (
         <div className="card-base product-card-admin">
-            <div className="row">
-                <div className="col-2 text-center border-right py-3">
+                <div className="text-center border-right py-3">
                     <img
                         src={product.imgUrl}
                         alt={product.name}
@@ -20,7 +19,7 @@ const Card = ({ product, onRemove } : Props) => {
                     />
                 </div>
                 <div className="col-7 py-3">
-                    <h3 className="product-card-name-admin">
+                    <h3 className="card-content product-card-name-admin">
                         {product.name}
                     </h3>
                     <ProductPrice price={product.price} />
@@ -47,7 +46,6 @@ const Card = ({ product, onRemove } : Props) => {
                         EXCLUIR
                     </Link>
                 </div>
-            </div>
         </div>
     );
 }
