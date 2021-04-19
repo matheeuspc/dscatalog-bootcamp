c2dc0149a304ef39a8f04eb7118b327f0657dd51
Matheus Cardoso <matheussp.cardoso@gmail.com>
2021-04-19 14:45:16 -0300

    implementing cd with expo updates

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,10 +1,25 @@
 import "react-native-gesture-handler";
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import * as Updates from 'expo-updates';
 import Routes from "./src/routes";
 
 
 const App: React.FC = () => {
+
+  async function checkUpdates() {
+    const update = await Updates.checkForUpdateAsync();
+
+    if (update.isAvailable){
+      await Updates.fetchUpdateAsync();
+      await Updates.reloadAsync();
+    }
+  }
+
+  useEffect(() => {
+    checkUpdates();
+  }, []);
+
   return (
     <NavigationContainer>
       <Routes />
